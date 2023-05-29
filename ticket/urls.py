b54ad80a37ca1a02d112c741254ca3e2b9f833ac
Avukonke Peter <11158856+avukonkepeter@users.noreply.github.com>
2023-05-29 09:06:33 +0200

    Add DefaultRouter to drf urls

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,10 +1,17 @@
-from rest_framework.routers import SimpleRouter
+from django.conf import settings
 
-from . import viewsets
+
+from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
+from ticket import viewsets
 
 
 router = SimpleRouter(trailing_slash=False)
+if settings.DEBUG:
+    router = DefaultRouter()
+
 router.register(r"events", viewsets.EventViewSet)
+router.register(r"tickettypes", viewsets.TicketTypeViewset)
 router.register(r"orders", viewsets.OrderViewSet)
 
 urlpatterns = router.urls
